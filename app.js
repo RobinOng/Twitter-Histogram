6412117bcebc38784209738f703cbd292de39714
robinkjong <robinkjong@hotmail.com>
2016-09-27 01:50:18 +1000

    Added Express library, request handler for "/"
    Port number is set to 3000

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express') // Import Express framework
+var port = 3000; // Port number
+
+var app = express();
+
+// Request handler for "/"
+app.get('/', function(request,response){
+    response.writeHead(200, {"Context-Type":"text/plain"});
+    response.write("Try /hello/:name");
+    response.end();
+})
+
+// Create HTTP Server and listen to port
+app.listen(port, function(){
+    console.log("Server is now running...");
+})
