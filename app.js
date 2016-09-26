2f8dbf78d58b7748acb73050c8ed85bd9c67b91f
robinkjong <robinkjong@hotmail.com>
2016-09-27 02:45:06 +1000

    Added request handler for "/hello/:name "

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/', function(request,response){
     response.end();
 })
 
+// Request handler for "/hello/:name
+app.get('/hello/:name', function(request,response){
+    response.writeHead(200, {"Context-Type":"text/plain"});
+    response.write("Hello " + request.params.name);
+    response.end();
+});
+
 // Create HTTP Server and listen to port
 app.listen(port, function(){
     console.log("Server is now running...");
