2fd55b832d0f4ad0a5e29698ad7364645abdee24
robinkjong <robinkjong@hotmail.com>
2016-09-27 04:00:20 +1000

    Added basic request handler for "/histogram/:twittername"

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express') // Import Express framework
+var twitter = require('twitter');// Import Twitter npm
+var env = require('dotenv').config({path: 'api.env'}); //Import dotenv npm
 var port = 3000; // Port number
 
 var app = express();
+
+var client = new twitter({
+    consumer_key: env.consumer_key,
+    consumer_secret: env.consumer_secret,
+    access_token_key: env.access_token_key,
+    access_token_secret: env.access_token_secret
+});
 
 // Request handler for "/"
 app.get('/', function(request,response){
@@ -10,10 +19,22 @@ app.get('/', function(request,response){
     response.end();
 })
 
-// Request handler for "/hello/:name
+// Request handler for "/hello/:name"
 app.get('/hello/:name(\\w+)', function(request,response){
     response.writeHead(200, {"Context-Type":"text/plain"});
     response.write("Hello " + request.params.name);
+    response.end();
+});
+
+// Request handler for "/histogram/:twittername"
+app.get('/histogram/:twittername(\\w+)', function(request,response) {
+    client.get('statuses/user_timeline', { screen_name: request.params.twittername, count: 1000 }, function(error, tweets) {
+        if (!error){
+            console.log(tweets[0]);
+        }
+    });
+    response.writeHead(200, {"Context-Type":"text/plain"});
+    response.write("Histogram " + request.params.twittername);
     response.end();
 });
 
